7efb1deff7af16b2ec4360656c82027220b9a176
Armaan-king <ARMAAN002@e.ntu.edu.sg>
2024-11-21 18:14:28 +0800

    Update MedicalRecordDoctorView.java

--- OOP Semester Project/src/hms/MedicalRecords/MedicalRecordDoctorView.java
+++ OOP Semester Project/src/hms/MedicalRecords/MedicalRecordDoctorView.java
@@ -2,6 +2,12 @@ package hms.MedicalRecords;
 
 import java.io.IOException;
 
+/**
+ * Interface representing the doctor's view of a patient's medical records.
+ * Provides methods for doctors to interact with a patient's medical records,
+ * such as adding diagnoses and treatment plans.
+ */
+
 public interface MedicalRecordDoctorView {
   void addDiagnosis(String diagnosis) throws IOException;
 
