8f0a0642bc3d0f06099f99b751509c1dcb7fca7f
Armaan-king <ARMAAN002@e.ntu.edu.sg>
2024-11-21 18:45:17 +0800

    Update Feedback.java Javadoc

--- OOP Semester Project/src/hms/Feedback.java
+++ OOP Semester Project/src/hms/Feedback.java
@@ -3,6 +3,10 @@ package hms;
 import java.io.*;
 import java.util.*;
 
+/* It allows patients to submit feedback, which includes comments and a rating.
+ * Administrators can view all feedback submitted by patients.
+ */
+
 public class Feedback {
     private static final String FEEDBACK_FILE = "C:\\Users\\welcome\\Desktop\\OOP---SC2002-Group-Project 3\\OOP---SC2002-Group-Project\\OOP Semester Project\\data\\feedback.csv";
     private String id; // Unique feedback ID
@@ -10,6 +14,15 @@ public class Feedback {
     private String comments;
     private int rating;
 
+    /**
+     * Constructs a new {@code Feedback} instance with the specified patient ID, comments, and rating.
+     * Generates a unique feedback ID upon creation.
+     *
+     * @param patientId The ID of the patient providing the feedback.
+     * @param comments  The comments provided by the patient.
+     * @param rating    The rating provided by the patient (e.g., 1-5).
+     */
+    
     public Feedback(String patientId, String comments, int rating) {
         this.id = "FB" + (new Random().nextInt(9000) + 1000);
         this.patientId = patientId;
@@ -17,6 +30,13 @@ public class Feedback {
         this.rating = rating;
     }
 
+    /**
+     * Saves the feedback to the feedback CSV file.
+     * If the file does not exist, it creates a new one with a header row.
+     *
+     * @throws IOException If an I/O error occurs during file operations.
+     */
+    
     public void save() throws IOException {
         File file = new File(FEEDBACK_FILE);
         boolean isNewFile = !file.exists();
@@ -28,6 +48,13 @@ public class Feedback {
             pw.printf("%s,%s,%s,%d%n", this.id, this.patientId, this.comments.replace(",", " "), this.rating);
         }
     }
+
+     /**
+     * Retrieves all feedback entries from the feedback CSV file.
+     *
+     * @return A list of strings, each representing a line from the feedback file.
+     * @throws IOException If an I/O error occurs during file reading.
+     */
 
     public static List<String> viewAllFeedback() throws IOException {
         List<String> feedbackList = new ArrayList<>();
