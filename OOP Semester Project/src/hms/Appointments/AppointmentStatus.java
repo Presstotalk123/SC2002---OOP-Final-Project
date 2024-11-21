9ab893f56fc25e17b896142aa2f2c1d4a265788d
Armaan-king <ARMAAN002@e.ntu.edu.sg>
2024-11-21 18:29:06 +0800

    Update AppointmentStatus.java

--- OOP Semester Project/src/hms/Appointments/AppointmentStatus.java
+++ OOP Semester Project/src/hms/Appointments/AppointmentStatus.java
@@ -1,8 +1,32 @@
 package hms.Appointments;
 
+/**
+ * Represents the status of an appointment in the hospital management system.
+ * An appointment can have one of the following statuses:
+ * <ul>
+ *   <li>{@link #CONFIRMED}</li>
+ *   <li>{@link #CANCELLED}</li>
+ *   <li>{@link #COMPLETED}</li>
+ *   <li>{@link #PENDING}</li>
+ * </ul>
+ */
+
 public enum AppointmentStatus {
+  /**
+    * Indicates that the appointment has been confirmed by the doctor.
+    */
   confirmed,
+  /**
+    * Indicates that the appointment has been cancelled.
+    */
   cancelled,
+  /**
+    * Indicates that the appointment has been completed.
+    */
+
   completed,
+  /**
+     * Indicates that the appointment is pending approval.
+     */
   pending
 }
