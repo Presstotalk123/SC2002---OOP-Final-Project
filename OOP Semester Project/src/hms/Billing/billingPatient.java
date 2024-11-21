22f6a54649a774ead669fc5bdeae6d34901a8f78
Armaan-king <ARMAAN002@e.ntu.edu.sg>
2024-11-21 18:20:38 +0800

    Update billingPatient.java Javadoc

--- OOP Semester Project/src/hms/Billing/billingPatient.java
+++ OOP Semester Project/src/hms/Billing/billingPatient.java
@@ -2,6 +2,19 @@ package hms.Billing;
 
 import java.io.IOException;
 
+/**
+ * Interface representing patient billing operations in the hospital management system.
+ * Provides methods for patients to interact with their bills, such as paying a bill.
+ */
+
 public interface billingPatient {
+    
+    /**
+     * Pays the bill with the specified bill ID.
+     *
+     * @param BillId The unique identifier of the bill to be paid.
+     * @throws IOException If an I/O error occurs during the payment process.
+     */
+
     void paybill(String BillId) throws IOException;
 }
