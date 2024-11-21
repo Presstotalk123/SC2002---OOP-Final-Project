4befa7e3cba8e0301da41911b7e709ff4c07b373
Armaan-king <ARMAAN002@e.ntu.edu.sg>
2024-11-21 18:57:13 +0800

    Update PrescriptionStatus.java

--- OOP Semester Project/src/hms/PrescriptionStatus.java
+++ OOP Semester Project/src/hms/PrescriptionStatus.java
@@ -1,5 +1,10 @@
 package hms;
 
+/**
+ * Enumeration representing the status of a prescription.
+ * It includes options for Pending and Dispensed statuses.
+ */
+
 public enum PrescriptionStatus {
   Pending,
   Dispensed
